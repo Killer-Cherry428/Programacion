dad12fcce4e7eb3e7558a37971424b4b09d8a76d
Miguel Ordoñez <miordonezh@unal.edu.co>
2024-11-27 15:33:51 -0500

    version3

---
+++ NewRepo/Quiz.py
@@ -0,0 +1,16 @@
+x=float(input("Ingrese la cantidad de hojas que dese tener. "))
+
+arbol=20 # ramas
+rama=25 #hojas
+hojasT=arbol*rama
+
+totalA=x/hojasT
+print("requiere un total de ",totalA," cantidad de arboles para conseguir el numero de hojas deseada")
+
+
+##interes
+deuda=float(input("Ingrese el valor de dinero que le presto su amigo. "))
+
+interes=deuda*0.05
+interesSem=deuda*7
+print("Al finalizar la semana debera pagar un total de: ",interesSem)
