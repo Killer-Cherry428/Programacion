2e6daf1ee0756a4f6ef813d71be1cafd91dbd292
Miguel Ordoñez <miordonezh@unal.edu.co>
2024-11-27 14:36:56 -0500

    version 2

--- NewRepo/prueba.py
+++ NewRepo/prueba.py
@@ -1,9 +1,63 @@
-edad=21
 
-if(edad==21):
-    print("Su edad es correcta")
+def funcion():
+    suma=1
+    a=0
+    b=a+suma
+    print(b)
+#--------------------------------------------------------------
+def f(x):
+    return x*x
 
-else:
-    print("Su edad es incorrecta")
+g=int(input("dato: "))
+print(f(g))
+funcion()
+#--------------------------------------------------------------
+def h(z):
+    return z*z
 
-    print("hola")
+print(h(2))
+#--------------------------------------------------------------
+def factorial():
+    s=10*9*8*7*6*5*4*3*2*1
+    print(s)
+
+factorial()
+#--------------------------------------------------------------
+def d(q):
+    return q**2
+
+print(d(10))
+
+#--------------------------------------------------------------
+import math #libreria de numeros irracionales y demas elementos matematicos
+def area(r):
+    area=math.pi*r**2
+    return area
+
+def perimetro(r):
+    peri=2*math.pi*r
+    return peri
+
+def volumen(r):
+    vol=(4/3)*math.pi*r**3
+    return vol
+
+
+t=float(input("Ingrese el radio: "))
+print("El área del circulo es: ",area(t))
+print("El perímetro del circulo es: ",perimetro(t))
+print("El vólumnen del circulo es: ",volumen(t))
+
+#--------------------------------------------------------------
+
+v=(5//2)*2
+print(v)
+
+#--------------------------------------------------------------
+
+def rectangulo(b,a):
+    p=b*a
+    return p
+
+
+print("el area es :",rectangulo(2,3))
