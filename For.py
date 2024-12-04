ca08a0a875606e64c2ffefb69eb8e521e19d08a3
Miguel Ordoñez <miordonezh@unal.edu.co>
2024-12-04 15:50:20 -0500

    version1

---
+++ For.py
@@ -0,0 +1,67 @@
+frutas=["Tomate de arbol","maracuya","Manzana"]
+
+print("\n")
+for f in frutas:
+    print(f)
+
+j=int(input("Ingrese el numero "))
+
+for d in range(j):
+    print(d)
+
+for e in range(2,-3,-1):
+    print(e)
+
+
+def FunFactorial():
+    print(d)
+
+
+for g in range(1,101):
+    h=g*g
+    print("El numero: ",g," y su cuadrado es: ",h)
+
+for w in range(1,1000,2):
+    print(w)
+
+for e in range(2,1002,2):
+    print(e)
+
+lo=20
+for rr in range(lo,1,-2):
+    print(rr)
+
+def factorial(n):
+    s=1
+    for dato in range (n,0,-1):
+        s=s*dato
+    return s
+
+fac=int(input("Ingrese numero "))
+print(factorial(fac))
+
+def exponencial(ex):
+    exp=2**ex
+    return exp
+
+hola=int(input("Ingrese dato del exponente"))
+print("El valor del exponente es",exponencial(hola))
+
+def exponencialIns(x,n):
+    xx=x**n
+    return xx
+
+hola1=int(input("Ingrese dato de la base"))
+hola2=int(input("Ingrese dato del exponente"))
+
+print("El valor es: ",exponencialIns(hola1,hola2))
+
+
+for tabla in range(1,11):
+    print("\nEsta es la tabla del ",tabla)
+
+    for tabla2 in range(1,11):
+        tabla3=tabla2*tabla
+        print(tabla," * ",tabla2," = ",tabla3)
+
+
